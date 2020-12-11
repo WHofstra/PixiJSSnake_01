9b81f30018cf193adc6de24acf59faab44508e31
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-12-11 01:59:14 +0100

    References PixiJS Succesful

--- testInputs/src/app.ts
+++ testInputs/src/app.ts
@@ -1,1 +1,7 @@
-console.log("code goes here");
+import * as PIXI from 'pixi.js';
+
+//Creates a Renderer Using WebGL
+const app = new PIXI.Application();
+
+//Creates a Canvas
+document.body.appendChild(app.view);
